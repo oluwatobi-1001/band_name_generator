32036c261ab1fc486b1ea157c1cb6ae4bff38e94
Oluwatobi Oyelude <oluwatobioye.tech@gmail.com>
2026-09-12 21:06:47 +0100

    Add band name generator script
    Takes a city and pet name as input and generates a fun band name suggestion

---
+++ band_name generator.py
@@ -0,0 +1,8 @@
+def main():
+    #the ue of ":" after main is very important and cannot be overemphasizedhence you will encounter a syntax error.
+    print("Welcome to the band name generator")
+    city= input("What's the name of the city you grew up in?")
+    pet= input("What's your pet's name?")
+    print(f"Your band name could be {city} {pet}")
+
+main()
